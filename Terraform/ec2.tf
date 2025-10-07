b68c2cd89cbf17a58c9f786078378e2edf270d41
Pritesh Kamble <priteshkamble13@gmail.com>
2025-10-07 15:52:33 +0530

    new

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -7,47 +7,6 @@ provider "aws" {
 resource "aws_instance" "webserver" {
     ami = "ami-08982f1c5bf93d976" 
     instance_type = "t2.micro"
-   vpc_security_group_ids = ["sg-0d186509748b7a67d",aws_security_group.sg_webserver.id ]
+   vpc_security_group_ids = ["sg-0d186509748b7a67d"]
 }
-
-user_data = <<-EOF 
-    #!/bin/sh
-    sudo -i
-    yum install nginx -y
-    systemctl start nginx
-    systemctl enable nginx
-    systemctl start mariadb
-    systemctl enable mariadb
-    bash /root/apache-tomcat-8.5.97/bin/catalina.sh start
-
-              EOF
-
-resource "aws_security_group" "sg_webserver" {
-
-    ingress {
-    description ="Allow http"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_block = ["0.0.0.0/0"]
-
-}
-
-ingrerss {
-    description = "All Traffic"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
-}
-
-egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
-
- }
-}
-
 
