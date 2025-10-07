a53019ae4e9f16b880f9c42766daf6676806020d
Pritesh Kamble <priteshkamble13@gmail.com>
2025-10-07 17:19:54 +0530

    ingress and egrees add

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_instance" "webserver" {
     ami = "ami-08982f1c5bf93d976" 
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
    vpc_security_group_ids = ["sg-0d186509748b7a67d"]
 }
 
